044532fdd31f783efce6191bbfd1a2d01ae6b22e
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2023-10-17 15:38:39 +0300

    Changes to experience page

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -5,12 +5,12 @@ export default function Experience() {
     return (
         <div className="bg-white dark:bg-black pb-20">
             <div className="max-w-6xl mx-auto h-15">
-                <h1 className="text-4xl md:text-4xl font-bold py-10 text-center text-black dark:text-white">
+                <h1 className="text-4xl font-bold py-10 text-center text-black dark:text-white">
                     Experience
                 </h1>
             </div>
             <div className="bg-white dark:bg-black">
-                <div className="grid grid-cols-1 bg-white dark:bg-black max-w-xl mx-auto pt-5">
+                <div className="grid grid-cols-1 bg-white dark:bg-black max-w-xl mx-auto">
                     {/* Experience card */}
                     {userData.experience.map((exp, idx) => (
                         <>
@@ -49,10 +49,12 @@ interface Props {
 const ExperienceCard: React.FC<Props> = ({ title, desc, year, company, companyLink }) => {
     return (
         <div className="relative experience-card border border-gray-200 dark:border-gray-600 p-4 rounded-md shadow-xl mx-4">
-            <h1 className="absolute -top-10 -left-2 md:-left-10 md:-top-10 text-2xl text-gray-200 font-bold text-gray-300 dark:text-gray-500">
-                {year}
-            </h1>
-            <h1 className="font-semibold text-xl text-black dark:text-white">{title}</h1>
+            <div className="flex justify-between items-baseline">
+                <h1 className="font-semibold text-xl text-black dark:text-white">{title}</h1>
+                <h1 className="text-sm md:text-xl font-bold text-gray-300 dark:text-gray-500">
+                    {year}
+                </h1>
+            </div>
             <a href={companyLink} className="text-gray-500">
                 {company}
             </a>
