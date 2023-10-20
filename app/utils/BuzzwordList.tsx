31a355b0adbef0b81a2375d33d109bd54c053ce9
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2023-10-20 14:05:02 +0300

    Changed color on buzzwords

--- app/utils/BuzzwordList.tsx
+++ app/utils/BuzzwordList.tsx
@@ -3,7 +3,7 @@ import { BuzzwordListProps } from '../types/types';
 
 const BuzzwordList: React.FC<BuzzwordListProps> = ({ buzzwords }) => {
   return (
-    <ul className='text-gray-500 dark:text-gray-400 grid grid-cols-1 gap-2 text-left text-secondary sm:grid-cols-2'>
+    <ul className='text-black dark:text-gray-400 grid grid-cols-1 gap-2 text-left text-secondary sm:grid-cols-2'>
       {buzzwords.map((buzzword, index) => (
         <li key={index} className="flex items-center space-x-3">
           <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
